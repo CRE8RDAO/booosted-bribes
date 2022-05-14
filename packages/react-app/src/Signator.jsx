b3063f0241c3eebcd9f878e4b8b9ba7aae31b71b
dunks411 <54425677+duncan4123@users.noreply.github.com>
2022-05-15 02:13:38 +1000

    update text

--- packages/react-app/src/Signator.jsx
+++ packages/react-app/src/Signator.jsx
@@ -1,11 +1,11 @@
-import { Alert, Button, Card, Checkbox, Input, notification, Radio, Space, Typography, Collapse, Select } from "antd"
+import { Alert, Button, Card, Code, Span, Checkbox, Input, notification, Radio, Space, Typography, Collapse, Select } from "antd"
 import { ethers } from "ethers"
 import React, { useEffect, useState } from "react"
 import { useHistory, useLocation } from "react-router-dom"
 import { useLocalStorage } from "./hooks"
 import { AddressInput } from "./components"
-import { useUserAddress } from "eth-hooks";
-import useBribe from "./hooks/Bribe";
+import { useUserAddress } from "eth-hooks"
+import useBribe from "./hooks/Bribe"
 import { calculateBasicBoosted } from "./helpers/bribeMath"
 import { amountToLpToGetBasicBoosted } from "./helpers/bribeMath"
 
@@ -43,21 +43,41 @@ const eip712Example = {
 }
 
 function BribeExplainer (props) {
-  const address = useUserAddress(props.injectedProvider);
+  const address = useUserAddress(props.injectedProvider)
 
-  const {cre8rScore,beetsScore} = useBribe(props.injectedProvider, address);
+  const { cre8rScore, beetsScore } = useBribe(props.injectedProvider, address)
 
   return (
-    <Card style={{ textAlign: "center", fontSize: "16px" }}>
+    <Card style={{ backgroundColor: "rgb(0 0 0 / 93%)", textAlign: "center", fontSize: "16px", padding:"50px" }}>
       <h1>For new voters it all starts with Basic Boosted</h1>
-      {beetsScore ? (
-        <span>For New Voters: LP at least ${amountToLpToGetBasicBoosted(beetsScore)} with {address}<br></br> To recieve Basic Boosted Bribe amount: {calculateBasicBoosted(beetsScore)}</span>
-      ) : (
-        <span> Connect your $FBEETS wallet to calculate your bribe payment options.</span>
-      )}
+      
+      <Text>
+        Your Basic Boosted Bribe amount is the hardest of all the bribes to calculate since you need to know: Your Voting Power / Total Voting Power * Dollar Value Of All Beets Emissions Directed By Votes. So this simple app does that for you.
+        {" "}
+      </Text>
+
+      <br></br>
+      <p>
+        {beetsScore ? (
+          <span>
+            For New Voters: LP at least ${Math.round(amountToLpToGetBasicBoosted(beetsScore))} with {address}
+            <br></br> To receive <Text type="success"> Basic Boosted </Text> Bribe amount: <Text type="success">${Math.round(calculateBasicBoosted(beetsScore))}</Text>
+          </span>
+        ) : (
+          <span> Connect your $FBEETS wallet to calculate your bribe payment options.</span>
+        )}
+      </p>
       <hr></hr>
-      <h3>This App is WIP: For info about the higher boosts pls check:  </h3><a href="https://cre8r.vip/boosted-bribes/" target="_blank">
-            🧱 Boosted Bribes™ {" "} </a>
+      <h1>Want more boost tho?</h1>
+      <p>TBH the rest of the Boosted Bribes Multipliers are a little bit more self explanatory.</p>
+      <p>Basically just compound your Bribes from last round and HODL to get 1.25x aka <Text type="success">Boosted Bribe.</Text> </p>
+      <p>OR increase you CRE8R holdings by 35% to get 1.35x Boost aka <Text type="success">Boosted Bonus</Text>. </p>
+
+      <hr></hr>
+      <h3>This App is WIP: For info about the higher boosts pls check: </h3>
+      <a href='https://cre8r.vip/boosted-bribes/' target='_blank'>
+        🧱 Boosted Bribes™{" "}
+      </a>
     </Card>
   )
 }
