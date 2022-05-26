98076d680e08768213d0ce30674415fb7fd375f6
dunks411 <54425677+duncan4123@users.noreply.github.com>
2022-05-26 17:45:17 +1000

    Update snapshot block & Total USD value for 100% of vote (needs to be done bi weekly)

--- packages/react-app/src/helpers/bribeMath.js
+++ packages/react-app/src/helpers/bribeMath.js
@@ -1,13 +1,13 @@
 const totalVotingPower = 48214146
 const thisWeeksBasicBribeAmount = 165295 // this should be total to work properly
 
-export function calculateBasicBoosted (beetsScore, totalVotingPower = 48214146 ,thisWeeksBasicBribeAmount = 165295) {
+export function calculateBasicBoosted (beetsScore, totalVotingPower = 48214146 ,thisWeeksBasicBribeAmount = 114551) {
   const basicBoostedForAddress = (beetsScore / totalVotingPower) * thisWeeksBasicBribeAmount
 
   return basicBoostedForAddress
 }
 
-export function amountToLpToGetBasicBoosted (beetsScore, totalVotingPower = 48214146,thisWeeksBasicBribeAmount = 165295) {
+export function amountToLpToGetBasicBoosted (beetsScore, totalVotingPower = 48214146,thisWeeksBasicBribeAmount = 114551) {
  return calculateBasicBoosted(beetsScore,totalVotingPower,thisWeeksBasicBribeAmount)*3
 }
 
